b32c7fbc3e762b272e237dd5ea76b8305d9f613c
Zivanic <zivanic23@gmail.com>
2017-04-04 12:26:40 +0200

    drugi side nav

--- berza-knjiga-admin/public_html/js/main.js
+++ berza-knjiga-admin/public_html/js/main.js
@@ -1,20 +1,20 @@
 $(".showNav").click(function () {
-    var viewportWidth = $(window).width();
-    if (viewportWidth < 768) {
-        
-            $(".side-nav").slideToggle("2000");
-            $(".showNav").toggleClass('buttonSlide');
-        
-    } else {
-        
-            $(".toggle-nav").toggleClass('col-sm-3');
-            $(".articles").toggleClass('col-sm-9');
-            $(".showNav").toggleClass('buttonSlide');
-            $(".side-nav li span:nth-of-type(2n)").toggleClass('showInline');
-            $(".toggle-nav").toggleClass('showInline');
-    }
+    $(".side-nav").toggleClass("showSideNav");
+    $(".showNav").toggleClass('buttonSlide');
+});
+$(".side-button").click(function () {
+
+    $(".toggle-nav").toggleClass('col-sm-3');
+    $(".articles").toggleClass('col-sm-9');
+    $(".side-nav li span:nth-of-type(2n)").toggleClass('showInline');
+    $(".toggle-nav").toggleClass('showInline');
+
 });
 $(".fa-search").click(function () {
     $(".search").toggleClass('showInline');
+
+});
+$(".fa-cogs").click(function () {
+    $(".nivelacija").toggleClass('showInline');
 
 });
